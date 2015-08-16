761fd363d89db5361bbd894737abf28693b2c722
Ronald van der Kooij <ronald@sharpsolutions.nl>
2015-08-16 11:33:24 +0200

    can pass new state into the helpers: "READY", "PENDING" and "FAILED"

--- __tests__/store-status-mixin.tests.js
+++ __tests__/store-status-mixin.tests.js
@@ -3,7 +3,7 @@ var Reflux = require('reflux');
 
 describe("Reflux store status mixin tests:", function() {
     
-    var triggeredState = null, unsubscribe;
+    var triggeredChange = null, unsubscribe;
 
     describe("when creating a store with the mixin", function() {
         var store = Reflux.createStore({
@@ -26,19 +26,19 @@ describe("Reflux store status mixin tests:", function() {
     describe("when calling the pending function", function() {
         var store = Reflux.createStore({
             mixins: [StoreStatusMixin],
-            goIntoPendingStatusPlease: function () {
-                this.pending();
+            goIntoPendingStatusPlease: function(data) {
+                this.pending(data);
             }
         });
         
-        beforeEach(function (done) {
+        beforeEach(function(done) {
             
-            unsubscribe = store.listen(function(state) {
-                triggeredState = state;
+            unsubscribe = store.listen(function(change) {
+                triggeredChange = change;
                 done();
             });
 
-            store.goIntoPendingStatusPlease();
+            store.goIntoPendingStatusPlease({ foo: "bar" });
         });
 
         it("it should put the store into the pending state", function() {
@@ -47,26 +47,27 @@ describe("Reflux store status mixin tests:", function() {
         });
 
         it("it should trigger the new state", function() {
-            expect(triggeredState.status).toBe(store.statusCodes.PENDING);            
+            expect(triggeredChange.foo).toBe("bar");   
+            expect(triggeredChange.status).toBe(store.statusCodes.PENDING);            
         });
     });
 
     describe("when calling the ready function", function() {
         var store = Reflux.createStore({
             mixins: [StoreStatusMixin],
-            goIntoReadyStatusPlease: function () {
-                this.ready();
+            goIntoReadyStatusPlease: function(newState) {
+                this.ready(newState);
             }
         });
 
-        beforeEach(function (done) {
+        beforeEach(function(done) {
             
             unsubscribe = store.listen(function(state) {
-                triggeredState = state;
+                triggeredChange = state;
                 done();
             });
             
-            store.goIntoReadyStatusPlease();
+            store.goIntoReadyStatusPlease({ foo: "bar" });
         });
 
         it("it should put the store into the ready state", function() {
@@ -74,26 +75,27 @@ describe("Reflux store status mixin tests:", function() {
         });
 
         it("it should trigger the new state", function() {
-            expect(triggeredState.status).toBe(store.statusCodes.READY);            
+            expect(triggeredChange.foo).toBe("bar");   
+            expect(triggeredChange.status).toBe(store.statusCodes.READY);            
         });
     });
 
     describe("when calling the failed function", function() {
         var store = Reflux.createStore({
             mixins: [StoreStatusMixin],
-            goIntoFailedStatusPlease: function () {
-                this.failed();
+            goIntoFailedStatusPlease: function(newState) {
+                this.failed(newState);
             }
         });
 
-        beforeEach(function (done) {
+        beforeEach(function(done) {
             
             unsubscribe = store.listen(function(state) {
-                triggeredState = state;
+                triggeredChange = state;
                 done();
             });
             
-            store.goIntoFailedStatusPlease();
+            store.goIntoFailedStatusPlease({ foo: "bar" });
         });
 
         it("it should put the store into the failed state", function() {
@@ -102,22 +104,23 @@ describe("Reflux store status mixin tests:", function() {
         });
 
         it("it should trigger the new state", function() {
-            expect(triggeredState.status).toBe(store.statusCodes.FAILED);            
+            expect(triggeredChange.foo).toBe("bar");   
+            expect(triggeredChange.status).toBe(store.statusCodes.FAILED);            
         });
     });
 
     describe("when calling the setStatus function", function() {
         var store = Reflux.createStore({
             mixins: [StoreStatusMixin],
-            goIntoStatusPlease: function () {
+            goIntoStatusPlease: function() {
                 this.setStatus(this.statusCodes.PENDING);
             }
         });
 
-        beforeEach(function (done) {
+        beforeEach(function(done) {
             
             unsubscribe = store.listen(function(state) {
-                triggeredState = state;
+                triggeredChange = state;
                 done();
             });
             
@@ -130,14 +133,14 @@ describe("Reflux store status mixin tests:", function() {
         });
 
         it("it should trigger the new state", function() {
-            expect(triggeredState.status).toBe(store.statusCodes.PENDING);            
+            expect(triggeredChange.status).toBe(store.statusCodes.PENDING);            
         });
     });
 
     describe("when resetting the status", function() {
         var store = Reflux.createStore({
             mixins: [StoreStatusMixin],
-            goIntoReadyStatusPlease: function () {
+            goIntoReadyStatusPlease: function() {
                 this.ready();
             }
         });
@@ -151,8 +154,54 @@ describe("Reflux store status mixin tests:", function() {
         });
     });
 
+    describe("When not passing any new state", function() {
+
+        it("it should ignore that state", function(done) {
+            var store = Reflux.createStore({
+                mixins: [StoreStatusMixin],
+                init: function() {
+                    this._state.foo = "bar";
+                },
+                goIntoReadyStatusPlease: function(newState) {
+                    this.ready(newState);
+                }
+            });
+            unsubscribe = store.listen(function(state) {
+                expect(state.status).toBe(store.statusCodes.READY);
+                expect(state.foo).toBe("bar");
+                done();
+            });
+            
+            store.goIntoReadyStatusPlease();
+        });
+    });
+
+    describe("When passing partial new state", function() {
+
+        it("it should only update that partial new state", function(done) {
+            var store = Reflux.createStore({
+                mixins: [StoreStatusMixin],
+                init: function() {
+                    this._state.fooOne = "bar1";
+                    this._state.fooTwo = "bar2";
+                },
+                goIntoReadyStatusPlease: function(newState) {
+                    this.ready(newState);
+                }
+            });
+            unsubscribe = store.listen(function(state) {
+                expect(state.status).toBe(store.statusCodes.READY);
+                expect(state.fooOne).toBe("bar1");
+                expect(state.fooTwo).toBe("barrr2");
+                done();
+            });
+            
+            store.goIntoReadyStatusPlease({ fooTwo:"barrr2" });
+        });
+    });
+
     beforeEach(function() {
-        triggeredState = null;
+        triggeredChange = null;
     });
 
     afterEach(function() {
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var objectAssign = require('object-assign');
+
 var statusCodes = {
     INITIAL: "INITIAL",
     READY: "READY",
@@ -6,28 +8,36 @@ var statusCodes = {
 };
 
 module.exports = {
-    init: function () {
+    init: function() {
         this._state = {
             status: statusCodes.INITIAL
         };
     },
-    pending: function () {
+    pending: function(newState) {
+        this._updateState(newState);
         this._triggerOnStatus(statusCodes.PENDING);
     },
-    ready: function () {
+    ready: function(newState) {
+        this._updateState(newState);
         this._triggerOnStatus(statusCodes.READY);
     },
-    failed: function () {
+    failed: function(newState) {
+        this._updateState(newState);
         this._triggerOnStatus(statusCodes.FAILED);
     },
-    setStatus: function (status) {
+    setStatus: function(status) {
         this._triggerOnStatus(status);
     },
     _triggerOnStatus: function(status) {
         this._state.status = status;
         this.trigger(this._state);
     },
-    resetToInitialStatus: function () {
+    _updateState: function(newState) {
+        if (newState) {
+            objectAssign(this._state, newState);
+        }
+    },
+    resetToInitialStatus: function() {
         this._state.status = statusCodes.INITIAL;
     },
     statusCodes: statusCodes
